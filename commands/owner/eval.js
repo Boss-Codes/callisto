f018485910982102a5bc3bfc980e25a2aef45064
Boss <boss@staff.libraryofcode.us>
2020-04-12 22:56:29 -0400

    eval works!

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -7,14 +7,26 @@ class Eval extends Command {
     }
     
     async execute(callisto, msg, args) { 
-        const fs = require('fs'); 
-        const wuper = 'super duper wuper'; 
-        const boss = 'Boss is a bad dev', 
-        const eris = require('eris'); 
-        if(!config.owner.includes(msg.author.id)) return; 
+        const content = msg.content.split(' ').slice(1).join(' ');
+            const result = new Promise((resolve, reject) => resolve(eval(content)));
 
-        const content = msg.content.spit(' ')
-    }
+        return result.then(output => { 
+            if(typeof output !== 'string') output = require('util').inspect(output, { 
+                depth: 0
+            });
+            if (output.includes(callisto.token)) output = output.replace(callisto.token, "Sorry sir/ma'am, I cannot do that."); 
+            if (output.length > 1990) console.log(output), output = 'The result of this eval is over 2000 characters long and cannot be sent, check the console for the output.'
+
+            return callisto.createMessage(msg.channel.id, `\`\`\`js\n${output}\`\`\``); 
+            }).catch(err => { 
+                console.error(err); 
+                err = err.toString(); 
+
+                if (err.includes(callisto.token)) err = err.replace(callisto.token, "So sorry sir/ma'am, cannot give yout that information."); 
+
+                return callisto.createMessage(msg.channel.id, `\`\`\`js\n${err}\`\`\``); 
+            });
+}
 }
 
 module.exports.cmd = Eval;
