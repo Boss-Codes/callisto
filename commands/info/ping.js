0cfc3abf8d8f8f8c86608eec97632111047aea83
Boss <boss@staff.libraryofcode.us>
2020-05-11 17:25:29 -0400

    Reverted again

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,15 +1,8 @@
 const { Command } = require('../../classes/Command.js'); 
 
 class Ping extends Command { 
-    constructor(){
-    super()
-    this.name = 'ping', 
-    this.description = 'Pings the bot.', 
-    this.usage = '!ping', 
-    this.example = null; 
-    this.permissions = 'User', 
-    this.aliases = ['pong'], 
-    this.module = 'Info'
+    constructor(name, description, usage, module){
+    super('ping', 'Pings the bot', '!ping', null, null, ['pong'], 'Info')
     }
 
     async execute(callisto, msg) { 
