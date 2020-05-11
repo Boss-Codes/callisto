03e0b2e70cd7eb08c45a43747d575b2c3cfee378
Boss <boss@staff.libraryofcode.us>
2020-05-11 17:25:05 -0400

    Let's try this again

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,8 +1,15 @@
 const { Command } = require('../../classes/Command.js'); 
 
 class Ping extends Command { 
-    constructor(name, description, usage, module){
-    super('ping', 'Pings the bot', '!ping', null, null, ['pong'], 'info')
+    constructor(){
+    super()
+    this.name = 'ping', 
+    this.description = 'Pings the bot.', 
+    this.usage = '!ping', 
+    this.example = null; 
+    this.permissions = 'User', 
+    this.aliases = ['pong'], 
+    this.module = 'Info'
     }
 
     async execute(callisto, msg) { 
