6f30bf9f7d37c4679fc4942d7f5eaa13995a4e82
Boss <boss@staff.libraryofcode.us>
2020-05-11 22:09:45 -0400

    Did some shit

---
+++ assets/global.js
@@ -0,0 +1,18 @@
+const global = {}
+global.version = 'v1.0.0'
+global.success = '<:callistoSuccess:699782547235799080>'
+global.error = '<:callistoError:699782782519607317>'
+global.online = '<:callistoOnline:699782570950524938>'
+global.idle = '<:callistoIdle:699782668165840947>'
+global.dnd = '<:callistoDnd:699782811686797432>'
+global.offline = '<:callistoOffline:699782609147789322>'
+
+module.exports = { 
+    version: version, 
+    success: success, 
+    error: error, 
+    online: online, 
+    idle: idle,
+    dnd: dnd,
+    offline: offline
+  }
