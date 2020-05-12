66b3e42d55384f7464d2579bb34f9d8ed94771db
Boss <boss@staff.libraryofcode.us>
2020-05-11 22:13:00 -0400

    Aliases

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,7 +5,7 @@ callisto.on('messageCreate', async msg => {
     let devPrefix = '$'
     const args = msg.content.slice(prefix.length).trim().split(/ +/g); 
     const cmd = args.shift().toLowerCase(); 
-    let command = callisto.commands.get(cmd); 
+    let command = callisto.commands.get(cmd) || callisto.commands.find(c => c.aliases && c.aliases.includes(cmd)); `    `
     if(!command) command = callisto.commands.get(callisto.aliases.get(cmd)); 
     if (cmd.length === 0) return; 
     if (!command) return; 
@@ -25,43 +25,6 @@ callisto.on('messageCreate', async msg => {
 
     if (command) { 
         
-        callisto.executeWebhook('698702660970348585', 'Bw1aSpxrK8SOxPj0K7HT3Hh_-uq8dXiKB3Ic0JDyHkvTnHYB8DS1ICbKHF1eQbPKfu64', { 
-
-            embeds: [
-             {
-                author: { 
-                    name: 'New Command Ran', 
-                    icon_url: callisto.user.avatarURL
-                }, 
-                color: '232550',
-    
-                fields: [
-                    { 
-                        name: 'Command', 
-                        value: `${command.name}`
-                    }, 
-                    { 
-                        name: 'Ran By', 
-                        value: `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`
-                        
-                    }, 
-                    { 
-                        name: 'Ran in Guild', 
-                        value: `${msg.channel.guild.name} (${msg.channel.guild.id})`
-                    },
-                    {
-                        name: 'Ran in Channel', 
-                        value: `${msg.channel.name} (${msg.channel.id})`
-                    },
-                    { 
-                        name: 'Time', 
-                        value: `${new Date()}`
-                    }
-                ]
-              }
-            ]
-        
-    })
     }
 })
 
