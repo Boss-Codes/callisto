bd9ee3c4789c7c315b71ed3f702cd3375953bc6a
Boss <boss@staff.libraryofcode.us>
2020-05-13 23:11:39 -0400

    Updated guild model

--- src/MongoDB/Models.js
+++ src/MongoDB/Models.js
@@ -1,11 +1,25 @@
 const { Schema, model } = require('mongoose')
 
 const GuildModel = new Schema({
+    guildID: String, 
+    guildOwner: String, 
+    guildOwnerID: Number, 
     prefix: {
         type: String,
         default: '!'
+    },
+    modlogChannel: String,
+    welcomeChannel: String,
+    loggingChannel: String,
+    messageLogChannel: String, 
+    welcomeMessage: { 
+        type: String,
+        default: "Welcome {user} to {server}!"
     }, 
-    guildID: String, 
+    modRoles: Array,
+    memberLogChannel: String, 
+    reportChannel: String
+
 }); 
 
 module.exports = model('Callisto', GuildModel)
