570e937067a4ed621e2120f759d25d6e54e213b0
Boss <boss@staff.libraryofcode.us>
2020-05-13 22:47:29 -0400

    Hid webhook token | Deleted config.json

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -6,7 +6,7 @@ let logDate = new Date().toLocaleDateString()
 callisto.on('ready', async () => { 
     callisto.editStatus('online', {name: 'users run !help', type: 2})
     
-    callisto.executeWebhook('698643966228431009', 'M6SS-bj4r-A87B4ExjlCNiYuMzmwAt92OhaoK3YVXQD6F6DoLg5X_-rjFypR9EBtvIZ4', { 
+    callisto.executeWebhook('698643966228431009', process.env.READY_WEBHOOK_TOKEN, { 
 
             embeds: [
              {
